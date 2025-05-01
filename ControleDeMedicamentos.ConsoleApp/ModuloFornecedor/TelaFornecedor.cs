436c84031bacbb628daa6cee9c90886bfde2879b
Victor R. Silva <victorr.silva005@gmail.com>
2025-05-01 14:46:59 -0300

    Implementado TelaFornecedor

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/TelaFornecedor.cs
@@ -0,0 +1,36 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedor
+{
+    public class TelaFornecedor : TelaBase<Fornecedor>
+    {
+        public TelaFornecedor(string nomeEntidade, RepositorioBase<Fornecedor> repositorio) : base(nomeEntidade, repositorio)
+        {
+        }
+
+        protected override void ExibirCabecalhoTabela()
+        {
+            Console.WriteLine("{0, -10} | {1, -30} | {2, -20} | {3, -20}", "Id", "Nome", "Telefone", "CNPJ");
+        }
+
+        protected override void ExibirLinhaTabela(Fornecedor fornecedor)
+        {
+            Console.WriteLine("{0, -10} | {1, -30} | {2, -20} | {3, -20}", fornecedor.Id, fornecedor.Nome, fornecedor.Telefone, fornecedor.CNPJ);
+        }
+
+        protected override Fornecedor ObterDados()
+        {
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine()!;
+
+            Console.Write("Digite o telefone: ");
+            string telefone = Console.ReadLine()!;
+
+            Console.Write("Digite o CNPJ: ");
+            string cnpj = Console.ReadLine()!;
+
+            Fornecedor fornecedor = new Fornecedor(nome, telefone, cnpj);
+            return fornecedor;
+        }
+    }
+}
