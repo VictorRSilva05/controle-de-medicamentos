7dcc185d73d09e8ef96bfe53b4e39984a31ea1c3
Victor R. Silva <victorr.silva005@gmail.com>
2025-05-01 14:14:43 -0300

    Adicionado repositórios base

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/Fornecedor.cs
@@ -0,0 +1,40 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedor
+{
+    public class Fornecedor : EntidadeBase<Fornecedor>
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string CNPJ { get; set; }
+
+        public Fornecedor(string nome, string telefone, string cnpj)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            CNPJ = cnpj;
+        }
+        public override void AtualizarRegistro(Fornecedor registroEditado)
+        {
+            Nome = registroEditado.Nome;
+            Telefone = registroEditado.Telefone;
+            CNPJ = registroEditado.CNPJ;
+        }
+
+        public override string Validar()
+        {
+            string erros = "";
+
+            if (string.IsNullOrWhiteSpace(Nome))
+                erros += "O campo Nome é obrigatório.\n";
+
+            if(string.IsNullOrWhiteSpace(Telefone))
+                erros += "O campo Telefone é obrigatório.\n";
+
+            if (string.IsNullOrWhiteSpace(CNPJ))
+                erros += "O campo CNPJ é obrigatório.\n";
+
+            return erros.Trim();
+        }
+    }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedor/RepositorioFornecedor.cs
@@ -0,0 +1,9 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloFornecedor
+{
+    public class RepositorioFornecedor : RepositorioBase<Fornecedor>
+    {
+
+    }
+}
