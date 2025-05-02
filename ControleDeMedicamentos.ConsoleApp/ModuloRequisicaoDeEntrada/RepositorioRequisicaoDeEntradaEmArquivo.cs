f29ccfbff3105c6376d0292feb9c1f6cd572aa50
Victor R. Silva <victorr.silva005@gmail.com>
2025-05-02 15:13:54 -0300

    Criada interface e repositório para classe RequisicaoDeEntrada

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloRequisicaoDeEntrada/RepositorioRequisicaoDeEntradaEmArquivo.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloRequisicaoDeEntrada
+{
+    public class RepositorioRequisicaoDeEntradaEmArquivo : RepositorioBaseEmArquivo<RequisicaoDeEntrada>, IRepositorioRequisicaoDeEntrada
+    {
+        public RepositorioRequisicaoDeEntradaEmArquivo(ContextoDados contexto) : base(contexto)
+        {
+        }
+
+        protected override List<RequisicaoDeEntrada> ObterRegistros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
